4b422f0ee8c874d614311df16275999601045b11
Serhii Basok <sergeybsk19@gmail.com>
2025-09-09 17:53:18 +0300

    create weather API

--- app/main.py
+++ app/main.py
@@ -1,6 +1,29 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+LINK = "http://api.weatherapi.com/v1/current.json"
+CITY = "Paris"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    key = os.getenv("API_KEY")
+    if not key:
+        raise ValueError("Key not found")
+
+    url = f"{LINK}?key={key}&q={CITY}"
+    response = requests.get(url, timeout=5)
+    data = response.json()
+
+    location = data["location"]["name"]
+    country = data["location"]["country"]
+    time = data["location"]["localtime"]
+    temp_c = data["current"]["temp_c"]
+    condition = data["current"]["condition"]["text"]
+
+    print(f"{location}/{country} {time} " f"Weather: {temp_c} Celsius, {condition}")
 
 
 if __name__ == "__main__":
